21b8f60064696804f690603dcfd95529dc30d468
ITemply <iamtemply@gmail.com>
2024-12-13 14:29:03 -0500

    game creation, game checks, id checks, dupe game hosting, game hosted times, randomized game ids

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('node:path')
+const crypto = require('crypto')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const fs = require('fs')
@@ -17,18 +18,20 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
-const getGameData = function(gameId) {
+const validChars = process.env.VALID_CHARS
+
+const getGameData = function(gameId = null) {
     const jsonData = fs.readFileSync('./appData/gameData.json')
     const parsedData = JSON.parse(jsonData)
 
-    if (parsedData['games'][gameId] == undefined || parsedData['games'][gameId] == null) {
-        return 'No Active Game'
+    if (parsedData['games'][gameId] == null) {
+        return parsedData['games']
     } else {
         return parsedData['games'][gameId]
     }
 }
 
-const setGameData = function(gameId, dataType, data = null) {
+const setGameData = async function(gameId, dataType, data = null) {
     const jsonData = fs.readFileSync('./appData/gameData.json')
     var parsedData = JSON.parse(jsonData)
     
@@ -38,6 +41,8 @@ const setGameData = function(gameId, dataType, data = null) {
 
             const jsonString = JSON.stringify(parsedData, null, 4)
             fs.writeFileSync('./appData/gameData.json', jsonString)
+
+            return gameId
         }
 
         return 'No Active Game'
@@ -55,6 +60,12 @@ const setGameData = function(gameId, dataType, data = null) {
             parsedData['games'][gameId]['gameUsers'].splice(userPosition, 1)
         } else if (dataType == 'gameQuestion') {
             parsedData['games'][gameId]['gameQuestion'] = data
+        } else if (dataType == 'setBlacklist') {
+            parsedData['games'][gameId]['gameBlacklist'].push(data)
+        } else if (dataType == 'setHost') {
+            parsedData['games'][gameId]['gameHost'] = data
+        } else if (dataType == 'setTime') {
+            parsedData['games'][gameId]['gameCreation'] = data
         }
     }
 
@@ -62,26 +73,102 @@ const setGameData = function(gameId, dataType, data = null) {
     fs.writeFileSync('./appData/gameData.json', jsonString)
 }
 
+const randomString = function(length) {
+    let result = ''
+    const characters = validChars
+    const charactersLength = characters.length
+    let counter = 0
+
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      counter += 1
+    }
+
+    return result
+}
+
+const hashString = function(string) {
+    return crypto.createHash('sha256').update(string).digest('hex')
+}
+
 app.get('/', async function(request, response) {
-    response.cookie('unfine-artist', 'Extremely')
+    const cookieCheck = request.cookies['unfine-artist']
+    if (cookieCheck) {
+        response.cookie('unfine-artist', hashString(randomString(25)))
+    } 
+
     console.log('Sending [GET]: /')
     response.render('index')
 })
 
 app.get('/host', async function(request, response) {
-    response.cookie('unfine-artist', 'Extremely')
+    const cookieCheck = request.cookies['unfine-artist']
+    console.log(cookieCheck)
+    if (cookieCheck == undefined || cookieCheck == null) {
+        response.cookie('unfine-artist', hashString(randomString(25)))
+    } 
+
     console.log('Sending [GET]: /host')
     response.render('host')
 })
 
+app.post('/api/getGameId', async function(request, response) {
+    var idRespone = 'Game Id In Use'
+
+    const hostinggameCheck = request.cookies.hostinggame
+    const hostId = request.cookies['unfine-artist']
+    const gameData = getGameData()
+    const seconds = new Date().getTime() / 1000
+
+    if (hostinggameCheck) {
+        response.send(JSON.stringify({
+            'Error':'You are already running a game at this time.'
+        }))
+        return
+    }
+
+    for (var entry in gameData) {
+        const hostMatch = gameData[entry]['gameHost']
+
+        if (hostId == hostMatch) {
+            response.send(JSON.stringify({
+                'Error':'You are already running a game at this time.'
+            }))
+            return
+        }
+    }
+
+    while (idRespone == 'Game Id In Use') {
+        const randomId = randomString(6)
+        idRespone = await setGameData(randomId, 'createGame')
+    }
+
+    await setGameData(idRespone, 'setHost', hostId)
+    await setGameData(idRespone, 'setTime', seconds)
+
+    response.cookie('hostinggame', idRespone)
+    response.send(JSON.stringify({
+        'gameId': idRespone
+    }))
+
+    console.log('Sending [POST]: /api/getGameId')
+})
+
 app.get('/join', async function(request, response) {
-    response.cookie('unfine-artist', 'Extremely')
+    const cookieCheck = request.cookies['unfine-artist']
+    if (cookieCheck) {
+        response.cookie('unfine-artist', hashString(randomString(25)))
+    } 
+
     console.log('Sending [GET]: /join')
     response.render('join')
 })
 
 app.get('/game', async function(request, response) {
-    response.cookie('unfine-artist', 'Extremely')
+    const cookieCheck = request.cookies['unfine-artist']
+    if (cookieCheck) {
+        response.cookie('unfine-artist', hashString(randomString(25)))
+    }
 
     const ingameCheck = request.cookies.ingame
     if (ingameCheck) {
--- public/js/host.js
+++ public/js/host.js
@@ -1,3 +1,26 @@
-const getGameId = function() {
-    
+const getGameId = async function() {
+    const response = await fetch('/api/getGameId', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        cache: 'default'
+    })
+
+    const rawResponse = await response.json()
+    const gameId = rawResponse.gameId
+
+    const error = rawResponse.Error
+
+    if (error) {
+        alert(error)
+        return
+    }
+
+    console.log(gameId)
+}
+
+document.getElementById('idButton').onclick = async function() {
+    await getGameId()
 }
